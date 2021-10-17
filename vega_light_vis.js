02387683689f823be4723d3af5eda5b25c225df6
zhuolangli <90895738+zhuolangli@users.noreply.github.com>
2021-10-17 19:16:09 +0800

    Add files via upload

---
+++ vega_light_vis.js
@@ -0,0 +1,24 @@
+var vg_1 = "Global_Population_Density.vg.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+
+}).catch(console.error);
+
+var vg_2 = "birth_rate_line_chart.vg.json";
+vegaEmbed("#birth_rate_line_chart", vg_2).then(function(result) {
+
+}).catch(console.error);
+
+var vg_3 = "death_rate_line_chart.vg.json";
+vegaEmbed("#death_rate_line_chart", vg_3).then(function(result) {
+
+}).catch(console.error);
+
+var vg_4 = "Life_Expactancy_bar_chart.vg.json";
+vegaEmbed("#Life_Expactancy_bar_chart", vg_4).then(function(result) {
+
+}).catch(console.error);
+
+var vg_5 = "growth_rate_Heat_map.vg.json";
+vegaEmbed("#growth_rate_Heat_map", vg_5).then(function(result) {
+
+}).catch(console.error);
