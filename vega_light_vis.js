08bee413875b65fbca9fdd05aab9a53d7494e4d0
zhuolangli <90895738+zhuolangli@users.noreply.github.com>
2021-10-17 19:15:26 +0800

    Delete vega_light_vis.js

--- vega_light_vis.js
+++
@@ -1,9 +0,0 @@
-var vg_1 = "Global_Population_Density.vg.json";
-vegaEmbed("#map", vg_1).then(function(result) {
-
-}).catch(console.error);
-
-var vg_2 = "birth_rate_line_chart.vg.json";
-vegaEmbed("#birth_rate_line_chart", vg_2).then(function(result) {
-
-}).catch(console.error);
